0a4c1b41e6428e73bf2eb102a59109babfa90e24
mfmehdi <mfmehdi2@gmail.com>
2019-11-26 07:58:21 +0100

    beesbeees done

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+	Grub.call(this); // Takes all the properties from Grub, If the values are updated then they are overridden with the new values in Bee cunstroctor
+	this.age = 5;
+	this.color = "yellow";
+	this.job = "keep on growing";
 };
 
+Bee.prototype = Object.create(Grub.prototype); // Extends the methods from Grub to Bee 
+Bee.prototype.constructor = Bee; // [instance].__proto__.constructor will point to the Bee constructor, not to the Grub constructor anymore
